89facdc7ffef2420ae106dea1423d4b6fe0199c8
ProsperieEli <ElijahProsperie@gmail.com>
2021-11-03 20:56:41 -0400

    test failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -26,21 +26,21 @@ describe('03_separation-of-concerns-demo routes', () => {
           id: '1',
           quantity: 10
         });
-
-    it('responds with an array of all orders', () => {
-          return request(app)
-          .get('/api/v1/orders')
-          .then(res => {
-            expect(res.body).toEqual([
-              {
-                id: '1',
-                quantity: 10
-              }
-            ])
-          })
-      
-        });
-      
       });
+      
+  });
+
+  it('responds with an array of all orders', () => {
+    return request(app)
+    .get('/api/v1/orders')
+    .then(res => {
+      expect(res.body).toEqual([
+        {
+          id: '1',
+          quantity: 10
+        }
+      ])
+    })
+
   });
 });
